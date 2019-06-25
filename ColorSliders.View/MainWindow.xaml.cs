e557d600a09e53726f16225a543acac4b9afbe98
filipPrivate <FBARSZCZEWSKI@OUTLOOK.COM>
2019-06-25 22:59:04 +0200

    Fixed saving last color

--- ColorSliders.View/MainWindow.xaml.cs
+++ ColorSliders.View/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using ColorSliders.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ColorSliders.View
 {
@@ -36,8 +25,7 @@ namespace ColorSliders.View
 
         private void Window_Closed(object sender, EventArgs e)
         {
-            //currentColor.SaveColor();
-            //  LastSettings.SaveColor(new RectangleColor(SlidersColor.R, SlidersColor.G, SlidersColor.B));
+            rectangleColor.SaveCurrentColor();
         }
     }
 }
