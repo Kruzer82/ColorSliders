8d5d2edee1025fe3f7e5004bb1c6e3d326029cdd
filipPrivate <FBARSZCZEWSKI@OUTLOOK.COM>
2019-06-25 22:10:47 +0200

    RGB class and DAL created

---
+++ ColorSliders.Model/LastColorDAL.cs
@@ -0,0 +1,22 @@
+﻿namespace ColorSliders.Model
+{
+    public static class LastColorDAL
+    {
+        public static RGB ReadLastRGB()
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+
+            return new RGB(settings.LastRed, settings.LastGreen, settings.LastBlue);
+        }
+
+        public static void WriteLastRGB(RGB color)
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+
+            settings.LastRed = color.Red;
+            settings.LastGreen = color.Green;
+            settings.LastBlue = color.Blue;
+            settings.Save();
+        }
+    }
+}
---
+++ ColorSliders.Model/RGB.cs
@@ -0,0 +1,16 @@
+﻿namespace ColorSliders.Model
+{
+    public class RGB
+    {
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+
+        public RGB(byte red, byte green, byte blue)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+        }
+    }
+}
