a1ee80ef3c260f0f53aa398e1d94f561312ce213
filipPrivate <FBARSZCZEWSKI@OUTLOOK.COM>
2019-06-26 16:58:31 +0200

    Added byte to double converter

---
+++ ColorSliders.View/Converters/ByteToDouble.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ColorSliders.View.Converters
+{
+    public class ByteToDouble : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)(byte)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (byte)(double)value;
+        }
+    }
+}
