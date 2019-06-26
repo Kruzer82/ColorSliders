54af7742d4bb0e982150657c384357d95dc6933b
filipPrivate <FBARSZCZEWSKI@OUTLOOK.COM>
2019-06-26 20:07:20 +0200

    Added conversion from Color to SolidColorBrush

---
+++ ColorSliders.View/Converters/ColorToSolidColorBrush.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ColorSliders.View.Converters
+{
+    public class ColorToSolidColorBrush : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color color = (Color)value;
+            return new SolidColorBrush(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //ignored
+            throw new NotImplementedException();
+        }
+    }
+}
