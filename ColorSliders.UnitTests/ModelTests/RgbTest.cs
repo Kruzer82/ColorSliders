c1b408fd682a021a9e6b878786810fefafb7fa16
filipPrivate <FBARSZCZEWSKI@OUTLOOK.COM>
2019-06-30 19:57:50 +0200

    Added RGB test

---
+++ ColorSliders.UnitTests/ModelTests/RgbTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ColorSliders.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ColorSliders.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RgbConstructorTest()
+        {
+            //arrange
+            byte r = 0;
+            byte g = 128;
+            byte b = 255;
+
+            //act
+            RGB color = new RGB(r, g, b);
+
+            //assert
+            Assert.AreEqual(r, color.Red,"Red value error.");
+            Assert.AreEqual(g, color.Green,"Red value error.");
+            Assert.AreEqual(b, color.Blue,"Red value error.");
+        }
+    }
+}
---
+++ ColorSliders.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ColorSliders.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ColorSliders.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a8165b7-5ab4-4a3e-9bb7-b077b48a879f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
